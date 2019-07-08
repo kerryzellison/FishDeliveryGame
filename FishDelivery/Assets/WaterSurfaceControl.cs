35c49a43f45e05ac4ffc4ca7fcc5f05e17c6ca60
rashfordcholston <carltongstale@gmail.com>
2019-07-08 15:26:48 +0200

    moved thomas's out of "homework level" folder

---
+++ FishDelivery/Assets/WaterSurfaceControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterSurfaceControl : MonoBehaviour
+{
+    public GameObject waterPlane;
+    Vector3 normal;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        normal = waterPlane.transform.position - transform.position;
+
+        waterPlane.transform.up = normal;
+    }
+}
