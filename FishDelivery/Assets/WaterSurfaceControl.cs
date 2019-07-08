dd373125c2e25c09697147a4383187cdc174b0bf
rashfordcholston <carltongstale@gmail.com>
2019-07-08 17:08:38 +0200

    failed at making water water in tank

--- FishDelivery/Assets/WaterSurfaceControl.cs
+++ FishDelivery/Assets/WaterSurfaceControl.cs
@@ -5,18 +5,54 @@ using UnityEngine;
 public class WaterSurfaceControl : MonoBehaviour
 {
     public GameObject waterPlane;
+
+    public GameObject WaterObj;
+    Mesh mesh;
+    List< Vector3> vertices;
     Vector3 normal;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        mesh = WaterObj.GetComponent<MeshFilter>().mesh;
+        vertices = GetTopCornerVertices(mesh);
     }
 
     // Update is called once per frame
     void Update()
     {
-        normal = waterPlane.transform.position - transform.position;
+        normal = CalculateWaterNormal(waterPlane.transform.position, transform.position);
+        
+        for (int i = 0; i < vertices.Count; i++)
+        {
+            //vertices[i].y;
+        }
 
+    }
+
+    private Vector3 CalculateWaterNormal(Vector3 from, Vector3 to)
+    {
+        Vector3 normal = (from - to).normalized;
         waterPlane.transform.up = normal;
+        return normal;
+    }
+
+    private List<Vector3> GetTopCornerVertices( Mesh mesh)
+    {
+        Vector3[] vertices = mesh.vertices;
+
+        List< Vector3> topVerts= new List<Vector3>();
+
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            if (vertices[i].y == 0.5)
+            {
+                topVerts.Add(transform.TransformPoint (vertices[i]));   
+            }
+        }
+
+
+        return topVerts;
+
     }
 }
