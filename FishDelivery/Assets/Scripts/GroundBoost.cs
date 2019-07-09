680938fd6a2d5c4ca06311471335f42fd8388db5
rashfordcholston <carltongstale@gmail.com>
2019-07-09 13:45:33 +0200

    Added Nitro amount and depletion

--- FishDelivery/Assets/Scripts/GroundBoost.cs
+++ FishDelivery/Assets/Scripts/GroundBoost.cs
@@ -6,28 +6,49 @@ public class GroundBoost : MonoBehaviour
 {
     //public RigidBodyInfo rbInfo;
     public Rigidbody carRB;
-    float boostDuration = 2000f, boostMultiplier = 2f;
+    public float boostDuration = 2000f, boostMultiplier = 2f, nitroDepletion = 15f, nitroStart, nitroMin = 0f, nitroMax = 500f, nitroAdd;
+
+    /* float nitroAmount {
+        get {return nitroAmount;}
+        set {_nitroAmount = Mathf.Clamp(nitroStart, nitroMin, nitroMax); }
+    } */
+    
+    public float _nitroAmount;
     private Coroutine boostCoroutine;
-    private bool boostOnce = true;
+    private bool boostOnce = true, nitroBoosting = false;
     // Start is called before the first frame update
     void Start()
     {
         carRB = GetComponent<Rigidbody>();
-        
+        float _nitroAmount = Mathf.Clamp(nitroStart, nitroMin, nitroMax);
+        //nitroAdd = 50;
     }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        
+    
+    void Update() {
+        if(nitroBoosting) {
+            _nitroAmount -= Time.deltaTime * nitroDepletion;
+        }
     }
 
     void OnTriggerEnter(Collider other) {
         
         if(other.tag == "BoostPad") {
-            StopBoost();
+            StopBoostPad();
             boostCoroutine = StartCoroutine(BoostPad());
         }
+        if(other.tag == "NitroPickup") {
+            if(_nitroAmount > nitroMax - nitroAdd) {
+                _nitroAmount = nitroMax;
+            }
+            else {
+            _nitroAmount += nitroAdd;
+            
+            }
+            other.gameObject.SetActive(false);
+            Debug.Log("Amount of nitro left = " + _nitroAmount); 
+
+        }
+
     }
 
     IEnumerator BoostPad() 
@@ -48,10 +69,21 @@ public class GroundBoost : MonoBehaviour
     }
 
     public void nitroBoost() {
+        Debug.Log("Nitro remaining = " + _nitroAmount);
+        if(_nitroAmount > 0f) {
+        nitroBoosting = true;
         carRB.velocity = carRB.velocity * boostMultiplier;
+        }
+        else {
+            _nitroAmount = 0;
+            nitroBoosting = false;
+        }
+
+        
+
     }
 
-    void StopBoost() {
+    void StopBoostPad() {
         if(boostCoroutine == null) {
             StopCoroutine(BoostPad());
             boostCoroutine = null;
