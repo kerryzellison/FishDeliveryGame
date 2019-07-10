e329ef92871b398adf7c74b113eb14c2c62144be
rashfordcholston <carltongstale@gmail.com>
2019-07-10 14:51:18 +0200

    3D UPGRADES word in Hub and car collider for waypoints

---
+++ FishDelivery/Assets/Scripts/FacePlayer2D.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlayer2D : MonoBehaviour
+{
+    Transform target;
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    void Update()
+    {
+        /// Rotate towards target on x and y axis, but maintain z rotation
+        transform.LookAt(new Vector3(target.position.x, transform.position.y, target.position.z));
+        //transform.eulerAngles = new Vector3(target.eulerAngles.x, target.eulerAngles.y, transform.eulerAngles.z);
+    }
+}
