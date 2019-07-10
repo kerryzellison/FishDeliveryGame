6359484027cf818737e18daa908889f20e0544e7
rashfordcholston <carltongstale@gmail.com>
2019-07-10 13:14:45 +0200

    Updated mission map

---
+++ FishDelivery/Assets/Scripts/MissionManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MissionManager : MonoBehaviour
+{
+    public AudioSource waterSplash;
+    public GameObject water;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided with something)");
+
+        if (collision.gameObject == water.gameObject)
+        {
+            waterSplash.Play();
+            ResetMission();
+            Debug.Log("Collided with water");
+        }
+    }
+
+    public void ResetMission()
+    {
+        SceneManager.LoadScene(Application.loadedLevel); //Load scene called Game
+    }
+}
